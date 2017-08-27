5d8d19e01cb8c455c678a63db811f3b233581081
Henrique Okomura <henrique.okomura@gmail.com>
2017-08-27 02:21:50 -0300

    Alert image

--- app/src/main/java/ssi/hackathon/diressom/MainActivity.java
+++ app/src/main/java/ssi/hackathon/diressom/MainActivity.java
@@ -1,13 +1,34 @@
 package ssi.hackathon.diressom;
 
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.StateListDrawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TransitionDrawable backgroundDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View layout = findViewById(R.id.background);
+        this.backgroundDrawable = (TransitionDrawable) layout.getBackground();
+    }
+
+    protected void onStart() {
+        super.onStart();
+        startAlert();
+    }
+
+    private void startAlert() {
+        backgroundDrawable.startTransition(2000);
+    }
+
+    private void finishAlert() {
+        backgroundDrawable.reverseTransition(2000);
     }
 }
